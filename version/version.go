8d8c938abd49d791f0976dcfd306e421955e27f8
Toshaan Bharvani <toshaan@vantosh.com>
2025-08-28 17:42:22 +0200

    move version printing to version folder
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

--- version/version.go
+++ version/version.go
@@ -11,8 +11,23 @@
 
 package version
 
+import (
+	"fmt"
+)
+
 var Version string
 var GitRevision string
 var GitBranch string
 var Builder string
 var Client string
+
+func PrintVersion() {
+	fmt.Printf("Check Kafka\n")
+	fmt.Printf("========================================\n")
+	fmt.Printf("License: Apache v2\n")
+	fmt.Printf("Version: %s\n", Version)
+	fmt.Printf("  git : (%s) %s\n", GitBranch, GitRevision)
+	fmt.Printf("Builder: %s\n", Builder)
+	fmt.Printf("Client: %s\n", Client)
+	fmt.Printf("========================================\n")
+}
